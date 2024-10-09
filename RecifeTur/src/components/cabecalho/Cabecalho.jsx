1399710328005c6ff2fed8782318e325567a144c
Eltinho <elton_13_cavalcante@hotmail.com>
2024-10-08 23:16:03 -0300

    Prévia do Cabeçalho

---
+++ RecifeTur/src/components/cabecalho/Cabecalho.jsx
@@ -0,0 +1,17 @@
+// Imagens
+import Logo from "../../assets/img/recifeTur.png";
+
+const Cabecalho = () => {
+  return (
+    <>
+      <header>
+        <img src={Logo} alt="Logo da RecifeTur" />
+        <nav>
+            
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Cabecalho;
