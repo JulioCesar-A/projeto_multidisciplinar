ff6a817c9d2697937b7dc1a41781df883510fa5c
Eltinho <elton_13_cavalcante@hotmail.com>
2024-10-09 14:20:43 -0300

    Páginas Corrigidas e estilos aplicados

--- RecifeTur/src/App.jsx
+++ RecifeTur/src/App.jsx
@@ -1,22 +1,24 @@
+
+
 // Componentes
 import Cabecalho from "./components/cabecalho/Cabecalho.jsx";
-import Rodape from "./components/rodape/Rodape.jsx";
 import SecaoInicial from "./components/secao_inicial/SecaoInicial.jsx";
 import SecaoSobre from "./components/secao_sobre/SecaoSobre.jsx";
 import SecaoComoFunciona from "./components/secao_como_funciona/SecaoComoFunciona.jsx";
 import SecaoCadastrarLocal from "./components/secao_cadastrar_local/SecaoCadastrarLocal.jsx";
+import Rodape from "./components/rodape/Rodape.jsx";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Cabecalho></Cabecalho>
-      <SecaoInicial></SecaoInicial>
-      <SecaoSobre></SecaoSobre>
-      <SecaoComoFunciona></SecaoComoFunciona>
-      <SecaoCadastrarLocal></SecaoCadastrarLocal>
-      <Rodape></Rodape>
+        <Cabecalho />
+          <SecaoInicial></SecaoInicial>
+          <SecaoSobre></SecaoSobre>
+          <SecaoComoFunciona></SecaoComoFunciona>
+          <SecaoCadastrarLocal></SecaoCadastrarLocal>
+        <Rodape />
     </>
   );
 }
---
+++ RecifeTur/src/components/login_cadastro/LoginCadastro.jsx
@@ -0,0 +1,13 @@
+import Cabecalho from "../cabecalho/Cabecalho";
+import Rodape from "../rodape/Rodape";
+
+const LoginCadastro = () => {
+  return (
+    <>
+      <Cabecalho></Cabecalho>
+      <Rodape></Rodape>
+    </>
+  );
+};
+
+export default LoginCadastro;
